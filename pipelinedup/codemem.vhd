fce355121a88ac933bcc38bc3d032e98012e00c8
singhalanirudh18 <singhalanirudh18@gmail.com>
2018-11-26 20:20:24 +0530

    codemem added

---
+++ pipelinedup/codemem.vhd
@@ -0,0 +1,26 @@
+library std;
+use std.standard.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity codemem is 
+	port(
+		 mem_a   : in std_logic_vector(15 downto 0);
+		 mem_out : out std_logic_vector(15 DOWNTO 0)); -- output
+end codemem;
+
+architecture structure of codemem is 
+
+
+type memarr is array(0 to 31) of std_logic_vector(15 downto 0);
+signal RAM : memarr := ("0001000000000010","0110000001011100","0001000000001000","0111000001011100", others => X"F000");
+signal addr : std_logic_vector(4 downto 0);
+
+begin
+
+addr <= mem_a(4 downto 0);
+mem_out <= RAM(to_integer(unsigned(addr)));
+
+end structure;
